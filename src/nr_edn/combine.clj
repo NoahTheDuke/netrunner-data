43ca7c96c4244d47a741a6e12867b5b239f2c211
Noah Bogart <noah.bogart@ebth.com>
2018-09-25 08:34:47 -0400

    Moved main to combine

--- src/nr_edn/combine.clj
+++ src/nr_edn/combine.clj
@@ -4,7 +4,7 @@
             [clojure.edn :as edn]
             [clojure.set :refer [rename-keys]]
             [org.httpkit.client :as http]
-            [nr-edn.utils :refer [cards->map]]))
+            [nr-edn.utils :refer [cards->map vals->vec]]))
 
 (defn read-edn-file
   [file-path]
@@ -90,3 +90,25 @@
             :uniqueness (:uniqueness card)})
          (map prune-null-fields)
          cards->map)))
+
+(defn compile-for-jnet
+  []
+  (try
+    (let [mwls (load-data "mwls" {:id :code
+                                  :date-start :date_start})
+          sides (load-data "sides")
+          factions (load-data "factions")
+          types (load-data "types")
+          subtypes (load-data "subtypes")
+          cycles (load-data "cycles")
+          sets (load-sets cycles)
+          cards (load-cards sides factions types subtypes sets)]
+      (spit (io/file "edn" "raw_data.edn")
+            (sorted-map
+              :mwls (vals->vec mwls)
+              :cycles (vals->vec cycles)
+              :sets (vals->vec sets)
+              :cards (vals->vec cards)))
+      (println "Generated raw_data.edn"))
+    (catch Exception e
+      (println "Import data failed:" (.getMessage e)))))
--- src/nr_edn/core.clj
+++ src/nr_edn/core.clj
@@ -1,27 +1,11 @@
 (ns nr-edn.core
   (:require [clojure.string :as string]
             [clojure.java.io :as io]
-            [nr-edn.combine :refer [load-data load-sets load-cards]]
-            [nr-edn.utils :refer [vals->vec]]))
+            [nr-edn.combine :refer [compile-for-jnet]]))
 
 (defn -main
   [& args]
   (try
-    (let [mwls (load-data "mwls" {:id :code
-                                  :date-start :date_start})
-          sides (load-data "sides")
-          factions (load-data "factions")
-          types (load-data "types")
-          subtypes (load-data "subtypes")
-          cycles (load-data "cycles")
-          sets (load-sets cycles)
-          cards (load-cards sides factions types subtypes sets)]
-      (spit (io/file "edn" "raw_data.edn")
-            (sorted-map
-              :mwls (vals->vec mwls)
-              :cycles (vals->vec cycles)
-              :sets (vals->vec sets)
-              :cards (vals->vec cards)))
-      (println "Generated raw_data.edn"))
+    (compile-for-jnet)
     (catch Exception e
       (println "Import data failed:" (.getMessage e)))))
